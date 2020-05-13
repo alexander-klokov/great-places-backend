c79e2672982a4af2da0521feb314d61a79efa516
Alexander Klokov <alexander.klokov@gmail.com>
2020-05-12 20:25:43 -0500

    handle errors

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -8,12 +8,26 @@ router.get('/:pid', (req, res, next) => {
     const placeId = req.params.pid
     const place = DUMMY_PLACES.find(place => place.id === placeId)
 
+    if (!place) {
+        const error = new Error ("Could not find a place for the provided id.")
+        error.code = 404
+
+        return next(error)
+    }
+
     res.json({place})
 })
 
 router.get('/user/:uid', (req, res, next) => {
     const userId = req.params.uid
     const places = DUMMY_PLACES.filter(place => place.creator === userId)
+
+    if (!places || !places.length) {
+        const error = new Error ("Could not find a place for the provided user id.")
+        error.code = 404
+        
+        return next(error)
+    }
 
     res.json({places})
 })
--- server.js
+++ server.js
@@ -7,4 +7,13 @@ const server = express()
 
 server.use('/api/places', placeRoutes)
 
+server.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error)
+    }
+    console.error('here', error)
+    res.status(error.code || 500)
+      .json({message: error.message || 'An unknown error occurred!'})
+})
+
 server.listen(5000)
