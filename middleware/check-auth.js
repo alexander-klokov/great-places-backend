6dc8424c27b07aa9da681d0da87d0fba2a891af5
Alexander Klokov <alexander.klokov@gmail.com>
2020-06-02 21:12:48 -0500

    protect route with auth middleware

---
+++ middleware/check-auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+const HttpError = require('../models/http-error')
+
+const {JWT_KEY} = process.env
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization // 'Bearer TOKEN'
+      .split(' ')[1]
+    if (!token) {
+        throw new Error('Authentification failed.')
+    }
+    const decodedToken = jwt.verify(token, JWT_KEY)
+    const {userId} = decodedToken
+    req.userData = {userId}
+    next()
+  } catch (e) {
+    return next(
+        new HttpError('Authentification failed', 401)
+    )
+  }
+}
